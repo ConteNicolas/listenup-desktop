a30aa2d04bc20cad281c34d085058b607cfcf2f4
Nicolas Conte <contenicolascv@gmail.com>
2025-12-27 17:08:18 -0300

    init project

---
+++ frontend/src/App.tsx
@@ -0,0 +1,11 @@
+import "@/index.css";
+
+function App() {
+  return (
+    <div className="min-h-screen bg-linear-to-br from-slate-50 to-slate-100 flex flex-col items-center justify-center p-8">
+      <h1>ListenUP Desktop</h1>
+    </div>
+  );
+}
+
+export default App;
